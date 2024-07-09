b7f5e4162ea6a657799f269663a1291276359a06
Muga Karmi Mugiisa Otieno <karmi.muga@strathmore.edu>
2024-07-09 11:42:34 +0300

    Revert "update"
    This reverts commit 50e89005dee2a3525cdb66fb6c8aec1f76c002c8.

--- resources/views/tracktrace/index.blade.php
+++ resources/views/tracktrace/index.blade.php
@@ -1,17 +1,11 @@
 @extends('layouts.app')
 @section('content')
-    <div class="flex justify-center items-center h-screen bg-blue-100"> <!-- Added bg-blue-100 for pale blue background -->
-        <div class="text-center">
-            <h1 class="text-3xl font-semibold mb-6">Track & Trace</h1>
-            <img src="{{ asset('tracking.jpg') }}" alt="Tracking Image" class="mx-auto mb-6" style="max-width: 200px;">
-            <form id="searchForm" action="{{ route('tracktrace.search') }}" method="GET" class="mb-6">
-                @csrf
-                <input type="text" name="search" placeholder="Enter AWB Number" class="border border-gray-300 px-4 py-2 rounded-md mr-2">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Submit
-                </button>
-            </form>
-        </div>
-    </div>
+    <center>
+    <h1> Track & Trace </h1>
+    <form id="searchForm" action="{{ route('tracktrace.search') }}" method="GET">   
+        @csrf
+        <input type="text" name="search" placeholder="Enter AWB Number">
+        <button type="submit">Submit</button>
+    </form>
+    </center>
 @endsection
-
