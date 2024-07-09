f97ae2daf99431a2ca107297c48a80f8b39cd770
Muga Karmi Mugiisa Otieno <karmi.muga@strathmore.edu>
2024-07-09 11:56:53 +0300

    Revert "commit"
    This reverts commit f61133fcd752143461968465520ad160276f51b1.

--- resources/views/tracked-cargo/create.blade.php
+++ resources/views/tracked-cargo/create.blade.php
@@ -5,25 +5,22 @@
 @endsection
 
 @section('content')
-    <section class="content container mx-auto py-8">
-        <div class="max-w-2xl mx-auto">
-            <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <div class="p-4 border-b">
-                    <h2 class="text-xl font-semibold text-gray-800">{{ __('Create') }} Tracked Cargo</h2>
-                </div>
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
 
-                <div class="p-4">
-                    <form method="POST" action="{{ route('tracked-cargos.store') }}" role="form" enctype="multipart/form-data">
-                        @csrf
+                <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title">{{ __('Create') }} Tracked Cargo</span>
+                    </div>
+                    <div class="card-body bg-white">
+                        <form method="POST" action="{{ route('tracked-cargos.store') }}"  role="form" enctype="multipart/form-data">
+                            @csrf
 
-                        @include('tracked-cargo.form')
+                            @include('tracked-cargo.form')
 
-                        <div class="flex justify-end mt-6">
-                            <button type="submit" class="inline-flex items-center justify-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                {{ __('Create') }}
-                            </button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/tracked-cargo/edit.blade.php
+++ resources/views/tracked-cargo/edit.blade.php
@@ -5,29 +5,25 @@
 @endsection
 
 @section('content')
-    <section class="content container mx-auto py-8">
-        <div class="max-w-2xl mx-auto">
-            <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <div class="p-4 border-b">
-                    <h2 class="text-xl font-semibold text-gray-800">{{ __('Update') }} Tracked Cargo</h2>
-                </div>
+    <section class="content container-fluid">
+        <div class="">
+            <div class="col-md-12">
 
-                <div class="p-4">
-                    <form method="POST" action="{{ route('tracked-cargos.update', $trackedCargo->id) }}" role="form" enctype="multipart/form-data">
-                        @method('PATCH')
-                        @csrf
+                <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title">{{ __('Update') }} Tracked Cargo</span>
+                    </div>
+                    <div class="card-body bg-white">
+                        <form method="POST" action="{{ route('tracked-cargos.update', $trackedCargo->id) }}"  role="form" enctype="multipart/form-data">
+                            {{ method_field('PATCH') }}
+                            @csrf
 
-                        @include('tracked-cargo.form')
+                            @include('tracked-cargo.form')
 
-                        <div class="flex justify-end mt-6">
-                            <button type="submit" class="inline-flex items-center justify-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                {{ __('Update') }}
-                            </button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 @endsection
-
